68c6da1a72b9b99453c6cb9b85dc9ded30bc63d1
Sonia Hussain <hussoni@amazon.com>
2023-03-02 12:09:30 +0000

    Adopting non-iterated array peak solution

--- src/array_peak/array_peak_finder.py
+++ src/array_peak/array_peak_finder.py
@@ -1,13 +1,26 @@
+from math import floor
 
 def find_peak(input_array: list(), swap_operations: list(list())):
     for op in swap_operations:
         item_to_move = input_array.pop(op[1])
         new_index = op[0]
         input_array.insert(new_index, item_to_move)
+    
+    sorted_array = sorted(input_array)
+    largest = get_largest_num(sorted_array)
+    return input_array.index(largest)
 
-    largest = input_array[0]
-    for i in range(1, len(input_array) - 1):
-        if (input_array[i] > largest):
-            largest = i
 
-    return largest
+def get_largest_num(sorted_array: list()):
+    if len(sorted_array) == 1: 
+        return sorted_array[0]
+
+    midpoint = floor(len(sorted_array) / 2)
+    largest = sorted_array[midpoint]
+
+    if sorted_array[midpoint + 1] and sorted_array[midpoint + 1] > largest:
+        largest = sorted_array[midpoint + 1]
+        return get_largest_num(sorted_array[midpoint + 1:])
+    elif sorted_array[midpoint - 1] and sorted_array[midpoint - 1] > largest:
+        largest = sorted_array[midpoint - 1]
+        return get_largest_num(sorted_array[:midpoint - 1])
