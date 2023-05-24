1a9a1e7b63b8ea8fb15be78e0f2e2f744945a4a8
ElephontDearie <sonia.hussain144@gmail.com>
2023-05-24 14:17:01 +0100

    Correct binary search

--- src/binary_search/index.py
+++ src/binary_search/index.py
@@ -1,25 +1,31 @@
 import decimal
+from enum import Enum
 
-decimal.getcontext().prec = 6
+decimal.getcontext().prec = 7
 
 initial_increment = 1
 
-def get_square_root(num: int, middle: float = None, last_increment = 1, last_try = None):
-    if (not middle):
-        middle = num / 2
-        print(middle)
-    
+Size = Enum('Size', ['BIG', 'SMALL'])
+default_increment = 0.000001
+
+def get_square_root(target: int, range: list, last_try: Size = None, increment: float = None):
+    print(range)
+    # if (range[0] == 1.772453): return 0
+
+    middle = round(((range[0] + range[1]) / 2), 6)
+    # print(middle)
     middle_calc = middle * middle
-    if (middle_calc == num):
-        return middle
-    
-    #  if middle_calc < num && last_try > num: last_increment / 2
-    elif (middle_calc < num):
-        return get_square_root(num, middle + 1)
-    
-    #  if middle_calc > num && last_try < num: last_increment * 2
-    elif (middle_calc > num):
-        return get_square_root(num, middle - 1)
 
+    if (middle_calc == target):
+        return round(middle, 6)
+    elif (range == [middle, range[1]] or range == [range[0], middle]):
+        return round(middle, 6)
+    
+    elif (middle_calc < target):
+        print("too little")
+        return get_square_root(target, [middle, range[1]])
 
+    elif (middle_calc > target):
+        print("too much")
+        return get_square_root(target, [range[0], middle])
 
--- test/test_square_root.py
+++ test/test_square_root.py
@@ -5,11 +5,14 @@ from src.binary_search.index import get_square_root
 class SquareRootFinderTest(unittest.TestCase):
 
     def test_works_for_whole_nums(self):
-        actual = get_square_root(4)
-        # print("\n")
-        # print(actual)
+        actual = get_square_root(4, [0, 4])
         self.assertAlmostEqual(actual, 2.000000, places = 6)
 
     def test_works_for_25(self):
-        actual = get_square_root(25)
-        self.assertEqual(actual, 5)
+        actual = get_square_root(25, [0, 25])
+        self.assertAlmostEqual(actual, 5.000000, places = 6)
+
+    def test_works_for_pi(self):
+        pi = 3.14159265359
+        actual = get_square_root(pi, [0, pi])
+        self.assertAlmostEqual(actual, 1.772454, places = 6)
